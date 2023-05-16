7ccd20ced9734a320014414008f98dd52c9baa2e
Ermakking <ermakking@yandex.ru>
2023-05-16 23:34:26 +0400

    correction of remarks

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,28 +4,28 @@ fun main() {
     var price: Int = 0
     val discount = 100
     val regularClient = ArrayList<String>()
-    regularClient.add("Kolya")
-    regularClient.add("Vova")
-    regularClient.add("Lisa")
-    regularClient.add("Nastya")
+    regularClient.add("Kolya@netology.ru")
+    regularClient.add("Vova@netology.ru")
+    regularClient.add("Lisa@netology.ru")
+    regularClient.add("Nastya@netology.ru")
 
     val name: String
     val scan = Scanner(System.`in`)
-    println("Введите имя")
+    println("Введите ваш адрес электронной почты")
     name = scan.nextLine()
     println("Введите сумму покупки")
     price = scan.nextInt()
-    if (price < 0) {
-        println("Сумма покупки не может быть меньше 0!")
-    } else if (price > 1000 && price <= 10000) {
-        price = price - discount
-    } else {
-        price = price - (price / 100 * 5)
+    when {
+        (price < 0) -> println("Сумма покупки не может быть меньше 0!")
+        (price in 0..1000) -> price = price
+        (price in 1001..10000) -> price = price - discount // знаю что можно короче, но пока мне так понятнее)
+        (price > 10001) -> price = price - (price / 100 * 5)
     }
     for (i in regularClient) {
         if (name == i) {
             price = price - (price / 100)
         }
     }
+
     println("Сумма вашей покупки составит: " + price)
 }
